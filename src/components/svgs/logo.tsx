8982df203bc971c664ae0f6ef09448f26e3c3c24
robshow <robsonaraujocarmo@hotmail.com>
2025-09-06 13:35:43 -0400

    fix: types

--- src/components/svgs/logo.tsx
+++ src/components/svgs/logo.tsx
@@ -1,16 +1,16 @@
 import Image from "next/image";
 import type { ComponentProps } from "react";
 
-interface LogoProps extends Omit<ComponentProps<typeof Image>, 'src' | 'alt'> {}
+interface LogoProps extends Omit<ComponentProps<typeof Image>, "src" | "alt"> {}
 
 export default function Logo(props: LogoProps) {
-	return (
-		<Image
-			src="/logo.svg"
-			alt="Abroad Handy logo"
-			width={48}
-			height={48}
-			{...props}
-		/>
-	);
+  return (
+    <Image
+      src="/logo.svg"
+      alt="Abroad Handy logo"
+      width={48}
+      height={48}
+      {...props}
+    />
+  );
 }
